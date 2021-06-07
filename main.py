2323cc298f1eddcee5042a569072a3082abf5c56
arup <dutta.arup22@gmail.com>
2021-06-07 18:51:05 +0530

    new line added

---
+++ main.py
@@ -0,0 +1,6 @@
+import pyttsx3
+speaker = pyttsx3.init()
+speaker.say('Hello,How are you today')
+speaker.say('I am fine, what about you')
+speaker.say('I am fine too...thank you')
+speaker.runAndWait()
